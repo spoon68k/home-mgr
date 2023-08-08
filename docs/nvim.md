455cd41dbed5ac0ff24124e777bea0539ee90576
Dan Spooner <8625813+spoon68k@users.noreply.github.com>
2023-08-08 17:13:57 +0100

    Configuration improvements for nixvim

---
+++ docs/nvim.md
@@ -0,0 +1,34 @@
+## NVIM Hints and Tips
+
+At this stage I've enabled a few very basic plugins not to get overwhelmed.
+
+Some stuff I've configured:
+
+leader      - set to ','
+leader f    - toggle NvimTree
+leader t    - open telescope
+noremap Y Y - make yank work the way I am used to (with the newline)
+
+
+### General VIM
+
+<ctrl>-w left|right -> to toggle between panels
+: <cmd> - runs the command in the shell
+:qa!    - quit all
+
+
+### Code Editing
+
+V - select lines visually
+> - indent
+< - outdent
+
+
+### NVimTree
+
+Remember that 'g?' opens help
+
+f - set a filter
+F - clear a filter
+< - up in the current group
+> - down in the current group
--- programs/nixvim.nix
+++ programs/nixvim.nix
@@ -1,12 +1,50 @@
-{ stdenv, config, pkgs, lib, ... }: {
+{ ... }: {
 
   # See https://github.com/nix-community/nixvim for configuration details
 
   programs.nixvim = {
     enable = true;
+
     plugins.lightline.enable = true;
     plugins.telescope.enable = true;
     plugins.nvim-tree.enable = true;
+    plugins.treesitter.enable = true;
+
+    plugins.lsp = {
+      enable = true;
+      enabledServers = [ "nil_ls" "pyright" ];
+      keymaps.lspBuf = {
+	"gd" = "definition";
+#	"gr" = "references";
+	"gt" = "type_definition";
+	"gi" = "implementation";
+	"R"  = "rename";
+	"K"  = "hover";
+      };
+    };
+
+    plugins.nvim-cmp = {
+      enable = true;
+      sources = [
+	{ name = "nvim_lsp"; }
+      ];
+      mapping = {
+	"<CR>" = "cmp.mapping.confirm({ select = true })";
+	"<Tab>" = {
+	  modes = [ "i" "s" ];
+	  action = "
+	    function(fallback) 
+	      if cmp.visible() then
+		cmp.select_next_item()
+	      else
+		fallback()
+	      end
+	    end
+	  ";
+	};
+      };
+    };
+
     colorschemes.gruvbox.enable = true;
 
     globals.mapleader = ",";
@@ -15,6 +53,34 @@
       number = true;
       relativenumber = true;
       shiftwidth = 2;
+    };
+
+    maps = {
+      normalVisualOp."Y" = "Y";
+      normal."<leader>f" = {
+	silent = true;
+	action = "<cmd>NvimTreeToggle<CR>";
+      };
+      normal."<leader>t" = {
+	silent = true;
+	action = "<cmd>Telescope<CR>";
+      };
+      normal."<leader>l" = {
+	silent = true;
+	action = "<cmd>Telescope buffers<CR>";
+      };
+      normal."<C-s>" = {
+	silent = true;
+	action = "<cmd>Telescope find_files<CR>";
+      };
+      normal."<C-f>" = {
+	silent = true;
+	action = "<cmd>Telescope live_grep<CR>";
+      };
+      normal."<leader>gr" = {
+	silent = true;
+	action = "<cmd>Telescope lsp_references<CR>";
+      };
     };
   };
 

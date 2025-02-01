5ab4e2681bd1d1f677291336e8ceb857d2d247b5
Dan Spooner <8625813+spoon68k@users.noreply.github.com>
2025-02-01 15:57:44 +0000

    Fix cmp configuration issue

--- dotfiles/nvim/lua/plugins/cmp.lua
+++ dotfiles/nvim/lua/plugins/cmp.lua
@@ -1,102 +1,92 @@
---
-
 return {
-    'hrsh7th/nvim-cmp',
+  'hrsh7th/nvim-cmp',
 
-    lazy = false,
+  lazy = false,
 
-    dependencies = {
-          'hrsh7th/cmp-nvim-lsp',
-          'hrsh7th/cmp-buffer',
-          'hrsh7th/cmp-path',
-          'hrsh7th/cmp-cmdline',
-          'L3MON4D3/LuaSnip',
-          'saadparwaiz1/cmp_luasnip',
-          'rafamadriz/friendly-snippets',
-    },
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets',
+    'onsails/lspkind-nvim', -- add this dependency
+  },
 
-    config = function()
+  config = function()
 
-        -- Load LuaSnip and snippets
-        require('luasnip.loaders.from_vscode').lazy_load()
+    -- Load LuaSnip snippets (from vscode-style snippet collections)
+    require('luasnip.loaders.from_vscode').lazy_load()
 
-        -- Set up nvim-cmp
-        local cmp = require('cmp')
-        local lspkind = require('lspkind')
+    local cmp = require('cmp')
+    local lspkind = require('lspkind')
 
-        cmp.setup({
+    cmp.setup({
+      snippet = {
+        -- Use LuaSnip for snippet expansion
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
 
-          snippet = {
-            -- REQUIRED: You must specify a snippet engine
-            expand = function(args)
-              lspkind.lsp_expand(args.body)  -- For LuaSnip users
-            end,
+      mapping = {
+        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      },
+
+      -- Define primary and secondary sources
+      sources = cmp.config.sources({
+        { name = "copilot" },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
+
+      -- Configure formatting via lspkind
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50,   -- maximum width of the popup menu
+          ellipsis_char = '...', -- character to show when truncating
+          symbol_map = {
+            Copilot = "",
           },
+        }),
+      },
+    })
 
-          mapping = {
-            ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-            ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.close(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          },
+    -- Filetype specific setup (example for gitcommit)
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+        { name = 'cmp_git' },
+      }, {
+        { name = 'buffer' },
+      })
+    })
 
-          sources = cmp.config.sources({
-            { name = "copilot" },
-            { name = 'nvim_lsp' },   -- LSP completions
-            { name = 'luasnip' },    -- Snippet completions
-          }, {
-            { name = 'buffer' },     -- Buffer word completions
-            { name = 'path' },       -- File path completions
-          }),
+    -- Use buffer source for '/' (search mode)
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
 
-          formatting = {
-            format = lspkind.cmp_format({
-              mode = 'symbol', -- show only symbol annotations
-              maxwidth = {
-                -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                -- can also be a function to dynamically calculate max width such as
-                -- menu = function() return math.floor(0.45 * vim.o.columns) end,
-                menu = 50, -- leading text (labelDetails)
-                abbr = 50, -- actual suggestion item
-              },
-              ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-              show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-              symbol_map = {
-                Copilot = ""
-              },
-              -- The function below will be called before any actual modifications from lspkind
-              -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            })
-            }
-        })
-
-        -- Set configuration for specific filetype (optional)
-        cmp.setup.filetype('gitcommit', {
-          sources = cmp.config.sources({
-            { name = 'cmp_git' },    -- Git completions
-          }, {
-            { name = 'buffer' },
-          })
-        })
-
-        -- Use buffer source for `/` (search mode)
-        cmp.setup.cmdline('/', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = 'buffer' }
-          }
-        })
-
-        -- Use cmdline & path source for ':' (command-line mode)
-        cmp.setup.cmdline(':', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = 'path' }
-          }, {
-            { name = 'cmdline' }
-          })
-        })
-    end
+    -- Use cmdline and path source for ':' (command-line mode)
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
+  end
 }
 

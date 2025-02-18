c251e7a67d7c110d5104c5528f317bcf9973c9f2
Dan Spooner <8625813+spoon68k@users.noreply.github.com>
2025-02-18 09:13:06 +0000

    Experimental WSL clipboard support

--- dotfiles/nvim/lua/core/clipboard.lua
+++ dotfiles/nvim/lua/core/clipboard.lua
@@ -2,19 +2,35 @@
 
 vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
 
--- if we are on WSL, then use clip.exe to allow copying to the windows side.
+-- if we are on WSL, then use clip.exe and powershell.exe to handle copy/pasting
 
-if vim.fn.executable(Env_Wsl_Clip) then
-  local opts = {
-    callback = function()
-      if vim.v.event.operator ~= "y" then
-        return
-      end
-      vim.fn.system(Env_Wsl_Clip, vim.fn.getreg(0))
-    end
-  }
+if vim.fn.executable(Env_Wsl_Clip) and vim.fn.executable(Env_Wsl_PowerShell) then
 
-  opts.group = vim.api.nvim_create_augroup("WSLYank", {})
-  vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+    vim.g.clipboard = {
+        name = "WslClipboard",
+        copy = {
+            ['+'] = Env_Wsl_Clip,
+            ['*'] = Env_Wsl_Clip,
+        },
+        paste = {
+            ['+'] = Env_Wsl_PowerShell .. ' -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("\r", ""))',
+            ['*'] = Env_Wsl_PowerShell .. ' -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("\r", ""))',
+        },
+        cache_enabled = 0,
+    }
+
 end
+
+--   local opts = {
+--     callback = function()
+--       if vim.v.event.operator ~= "y" then
+--         return
+--       end
+--       vim.fn.system(Env_Wsl_Clip, vim.fn.getreg(0))
+--     end
+--   }
+-- 
+--   opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+--   vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+-- end
 

c3409efa72bad3858b249fc8fd9b44c678b95015
Dan Spooner <8625813+spoon68k@users.noreply.github.com>
2025-03-10 13:48:29 +0000

    Update calendar plugin to work with new multi-vault support

--- dotfiles/nvim/lua/plugins/calendar_nav.lua
+++ dotfiles/nvim/lua/plugins/calendar_nav.lua
@@ -1,9 +1,23 @@
+
+-- Find the dailiest directory for the active vault
+local dailies_dir = nil
+for name, note in pairs(Env_Obsidian_Vaults) do
+    if vim.env.NVIM_ACTIVE_VAULT == name then
+        dailies_dir = string.format("%s/%s/dailies", Env_Home, note:match("(.*/)"))
+    end
+end
+
+-- If there is no selection (or it is invalid), disable the plug-in
+if dailies_dir == nil then
+    return {}
+end
+
 return {
     "spoon68k/calendar-nav",
     lazy = false,
     config = function()
         require("calendar-nav").setup({
-            dailies_dir = string.format("%s/%s/dailies", Env_Home, Env_Note_Vault),
+            dailies_dir = dailies_dir,
             border = "rounded",
         })
     end
